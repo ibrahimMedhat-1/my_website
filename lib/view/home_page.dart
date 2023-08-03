7494907aa964c9f1659c31440ac488b0d8ec4cdf
ibrahimmedhat <doha.kroz0@gmail.com>
2023-08-03 19:56:46 +0300

    Home Page Edit

--- lib/view/home_page.dart
+++ lib/view/home_page.dart
@@ -17,95 +17,92 @@ class HomePage extends StatelessWidget {
       ),
       body: Column(
         children: [
-          Padding(
-            padding: const EdgeInsets.symmetric(vertical: 0),
-            child: Container(
-              height: 550,
-              decoration: BoxDecoration(
-                color: color,
-              ),
-              child: Row(
-                children: [
-                  // Name and Data
-                  Expanded(
-                    child: Padding(
-                      padding: const EdgeInsets.only(left: 80),
-                      child: Column(
-                        mainAxisSize: MainAxisSize.min,
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          Text(
-                            'HELLO',
-                            style: Theme.of(context).textTheme.bodySmall,
-                          ),
-                          const SizedBox(
-                            height: 10,
-                          ),
-                          Row(
-                            mainAxisSize: MainAxisSize.min,
-                            children: [
-                              Text(
-                                'I\'m',
-                                style: Theme.of(context).textTheme.bodyLarge,
-                              ),
-                              Text(
-                                ' Ibrahim',
-                                style: Theme.of(context).textTheme.bodyLarge!.copyWith(fontWeight: FontWeight.bold),
-                              ),
-                            ],
-                          ),
-                          const SizedBox(
-                            height: 10,
-                          ),
-                          Text(
-                            'Medhat',
-                            style: Theme.of(context).textTheme.bodyLarge!.copyWith(fontWeight: FontWeight.bold, fontSize: 70),
-                          ),
-                          const SizedBox(
-                            height: 10,
-                          ),
-                          Text(
-                            'Software Engineer / Developer',
-                            style: Theme.of(context).textTheme.bodySmall,
-                          ),
-                          const SizedBox(
-                            height: 10,
-                          ),
-                          Row(
-                            mainAxisSize: MainAxisSize.min,
-                            children: [
-                              MaterialButton(
-                                onPressed: () {},
-                                child: const Text('Hire Me'),
-                              ),
-                              MaterialButton(
-                                onPressed: () {},
-                                child: const Text('Portfolio'),
-                              ),
-                            ],
-                          ),
-                        ],
-                      ),
-                    ),
-                  ),
-                  // Profile Picture
-                  const Expanded(
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.end,
+          Container(
+            height: 550,
+            decoration: BoxDecoration(
+              color: color,
+            ),
+            child: Row(
+              children: [
+                // Name and Data
+                Expanded(
+                  child: Padding(
+                    padding: const EdgeInsets.only(left: 80),
+                    child: Column(
+                      mainAxisSize: MainAxisSize.min,
+                      crossAxisAlignment: CrossAxisAlignment.start,
                       children: [
-                        Padding(
-                          padding: EdgeInsets.only(right: 80),
-                          child: CircleAvatar(
-                            minRadius: 80,
-                            maxRadius: 120,
-                            backgroundImage: AssetImage('assets/profileImage.jpg'),
-                          ),
-                        )
+                        Text(
+                          'HELLO',
+                          style: Theme.of(context).textTheme.bodySmall,
+                        ),
+                        const SizedBox(
+                          height: 10,
+                        ),
+                        Row(
+                          mainAxisSize: MainAxisSize.min,
+                          children: [
+                            Text(
+                              'I\'m',
+                              style: Theme.of(context).textTheme.bodyLarge,
+                            ),
+                            Text(
+                              ' Ibrahim',
+                              style: Theme.of(context).textTheme.bodyLarge!.copyWith(fontWeight: FontWeight.bold),
+                            ),
+                          ],
+                        ),
+                        const SizedBox(
+                          height: 10,
+                        ),
+                        Text(
+                          'Medhat',
+                          style: Theme.of(context).textTheme.bodyLarge!.copyWith(fontWeight: FontWeight.bold, fontSize: 70),
+                        ),
+                        const SizedBox(
+                          height: 10,
+                        ),
+                        Text(
+                          'Software Engineer / Developer',
+                          style: Theme.of(context).textTheme.bodySmall,
+                        ),
+                        const SizedBox(
+                          height: 10,
+                        ),
+                        Row(
+                          mainAxisSize: MainAxisSize.min,
+                          children: [
+                            MaterialButton(
+                              onPressed: () {},
+                              child: const Text('Hire Me'),
+                            ),
+                            MaterialButton(
+                              onPressed: () {},
+                              child: const Text('Portfolio'),
+                            ),
+                          ],
+                        ),
                       ],
                     ),
                   ),
-                ],
-              ),
+                ),
+                // Profile Picture
+                const Expanded(
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.end,
+                    children: [
+                      Padding(
+                        padding: EdgeInsets.only(right: 80),
+                        child: CircleAvatar(
+                          minRadius: 80,
+                          maxRadius: 120,
+                          backgroundImage: AssetImage('assets/profileImage.jpg'),
+                        ),
+                      )
+                    ],
+                  ),
+                ),
+              ],
             ),
           ),
         ],
