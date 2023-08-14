7eabcc134315626ee7627ff27dd13a08fb375f9b
ibrahimmedhat <doha.kroz0@gmail.com>
2023-08-14 20:19:36 +0300

    finish mobile and web handling

--- lib/layout/layout.dart
+++ lib/layout/layout.dart
@@ -2,8 +2,10 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:my_website/view_model/layout_cubit/layout_cubit.dart';
 
+import '../shared/components/animated_contact_icons.dart';
 import '../shared/components/animated_default_button.dart';
 import '../shared/style.dart';
+import '../view_model/home_page_cubit/home_page_cubit.dart';
 
 class Layout extends StatelessWidget {
   const Layout({super.key});
@@ -132,7 +134,190 @@ class Layout extends StatelessWidget {
               Expanded(child: cubit.pages[cubit.page]),
             ],
           )),
-          drawer: Container(),
+          drawer: Container(
+            color: Colors.white,
+            width: 300,
+            child: Column(
+              children: [
+                Container(
+                  height: 150,
+                  width: 300,
+                  color: Colors.blue,
+                  child: Center(
+                    child: Text(
+                      'Ibrahim Medhat',
+                      style: Theme.of(context).textTheme.bodyLarge!.copyWith(
+                            color: Colors.white,
+                          ),
+                    ),
+                  ),
+                ),
+
+                ///home
+                MaterialButton(
+                  onPressed: () {
+                    cubit.changePage(0);
+                    Navigator.pop(context);
+                  },
+                  child: Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+                    child: Row(
+                      children: [
+                        Icon(
+                          Icons.home,
+                          size: 30,
+                          color: cubit.isSelected[0] ? Colors.blue : Colors.grey,
+                        ),
+                        const SizedBox(width: 20),
+                        Text(
+                          'Home',
+                          style: TextStyle(
+                            fontSize: 20,
+                            color: cubit.isSelected[0] ? Colors.blue : Colors.grey,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+
+                ///about
+                MaterialButton(
+                  onPressed: () {
+                    cubit.changePage(1);
+                    Navigator.pop(context);
+                  },
+                  child: Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+                    child: Row(
+                      children: [
+                        Icon(
+                          Icons.person,
+                          size: 30,
+                          color: cubit.isSelected[1] ? Colors.blue : Colors.grey,
+                        ),
+                        const SizedBox(width: 20),
+                        Text(
+                          'About',
+                          style: TextStyle(
+                            fontSize: 20,
+                            color: cubit.isSelected[1] ? Colors.blue : Colors.grey,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+
+                ///projects
+                MaterialButton(
+                  onPressed: () {
+                    cubit.changePage(2);
+                    Navigator.pop(context);
+                  },
+                  child: Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+                    child: Row(
+                      children: [
+                        ImageIcon(
+                          const AssetImage('assets/contact_icons/github.png'),
+                          size: 30,
+                          color: cubit.isSelected[2] ? Colors.blue : Colors.grey,
+                        ),
+                        const SizedBox(width: 20),
+                        Text(
+                          'Projects',
+                          style: TextStyle(
+                            fontSize: 20,
+                            color: cubit.isSelected[2] ? Colors.blue : Colors.grey,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+
+                ///contact
+                MaterialButton(
+                  onPressed: () {
+                    cubit.changePage(3);
+                    Navigator.pop(context);
+                  },
+                  child: Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+                    child: Row(
+                      children: [
+                        Icon(
+                          Icons.perm_contact_calendar_rounded,
+                          size: 30,
+                          color: cubit.isSelected[3] ? Colors.blue : Colors.grey,
+                        ),
+                        const SizedBox(width: 20),
+                        Text(
+                          'Home',
+                          style: TextStyle(
+                            fontSize: 20,
+                            color: cubit.isSelected[3] ? Colors.blue : Colors.grey,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+                const Divider(
+                  thickness: 1,
+                  color: Colors.black45,
+                ),
+                const SizedBox(height: 20),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    ContactIcon(
+                      icon: 'assets/contact_icons/gitHub.png',
+                      onTap: () {
+                        HomePageCubit.get(context).launch(url: 'https://github.com/ibrahimMedhat-1');
+                      },
+                    ),
+                    const SizedBox(width: 20),
+                    ContactIcon(
+                      icon: 'assets/contact_icons/linkedin.png',
+                      onTap: () {
+                        HomePageCubit.get(context).launch(url: 'https://www.linkedin.com/in/ibrahim-medhat-612043224/');
+                      },
+                    ),
+                    const SizedBox(width: 20),
+                    ContactIcon(
+                      icon: 'assets/contact_icons/whatsapp.png',
+                      onTap: () {
+                        HomePageCubit.get(context).launch(url: 'https://wa.me/201553708872?text=Hi,%20I%20wanna%20talk%20about%20business.');
+                      },
+                    ),
+                    const SizedBox(width: 20),
+                    ContactIcon(
+                      icon: 'assets/contact_icons/facebook.png',
+                      onTap: () {
+                        HomePageCubit.get(context).launch(url: 'https://www.facebook.com/Ibrahimmedha?mibextid=ZbWKwL');
+                      },
+                    ),
+                    const SizedBox(width: 20),
+                    ContactIcon(
+                      icon: 'assets/contact_icons/instagram.png',
+                      onTap: () {
+                        HomePageCubit.get(context).launch(url: 'https://instagram.com/ibrahemmedhat2014?igshid=MzNlNGNkZWQ4Mg==');
+                      },
+                    ),
+                  ],
+                ),
+                const SizedBox(height: 20),
+                Text(
+                  'Ibrahim Medhat copyright 2023 developed by Ibrahim Medhat',
+                  style: Theme.of(context).textTheme.bodySmall!.copyWith(
+                        fontSize: 10,
+                      ),
+                ),
+              ],
+            ),
+          ),
           // body: cubit.pages[1],
         );
       },
